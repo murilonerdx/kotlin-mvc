82a71c6d43cbec8dfb9594a4500b610148ef9d30
Murilonerdx <mu-silva@outlook.com>
2021-06-21 18:39:21 -0300

    Criando o Model para o cadastro de Usuario

---
+++ src/main/kotlin/com/murilonerdx/blog/IndexController.kt
@@ -0,0 +1,14 @@
+package com.murilonerdx.blog
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+
+@Controller
+class IndexController {
+
+    @RequestMapping("/",method=[RequestMethod.GET])
+    fun index():String{
+        return "index";
+    }
+}
--- src/main/kotlin/com/murilonerdx/blog/SignUpController.kt
+++ src/main/kotlin/com/murilonerdx/blog/SignUpController.kt
@@ -2,15 +2,23 @@ package com.murilonerdx.blog
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 
 @Controller
-class SignUp {
+@RequestMapping("/signup")
+class SignUpController {
 
-    @RequestMapping("/signup", method = [RequestMethod.GET])
-    fun signup(): String{
+    @GetMapping
+    fun form(): String{
         return "signup";
+    }
+
+    @PostMapping
+    fun save(user: User, confirmPassword: String):String{
+        print("form $user")
+        return "redirect:/index";
     }
 
 }
