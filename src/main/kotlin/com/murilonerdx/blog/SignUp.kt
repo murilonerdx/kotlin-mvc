258a1ef68933c6620f20e37f73915d7c878b8d01
Murilonerdx <mu-silva@outlook.com>
2021-06-21 18:01:15 -0300

    Criando o controller para o cadastro de usuario

---
+++ src/main/kotlin/com/murilonerdx/blog/SignUp.kt
@@ -0,0 +1,15 @@
+package com.murilonerdx.blog
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+
+@Controller
+class SignUp {
+
+    @RequestMapping("/signup", method = [RequestMethod.GET])
+    fun signup(): String{
+        return "signup";
+    }
+
+}
